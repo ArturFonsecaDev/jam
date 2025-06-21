7afbdef6ae38b59a2cae12f363e77ceafcdd5d7f
ArturFonsecaDev <sgsartur@hotmail.com>
2025-06-21 18:22:41 -0400

    feat: controller for JamRoom added

--- app/Http/Controllers/JamRoomController.php
+++ app/Http/Controllers/JamRoomController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 class JamRoomController extends Controller
 {
-    //
+    private function getCurrentUser()
+    {
+        return Auth::user();
+    }
 }
