02fadc55e0e1dc5e96f22b183f1329eafb059534
SaraMegumi <megumisarasakai@gmail.com>
2025-06-21 19:24:03 -0300

    feat: add pagina, add rota e estilizei a pagina

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,14 +4,19 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, Plus, Music } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
+        title: 'Criar Jam',
         href: '/dashboard',
-        icon: LayoutGrid,
+        icon: Plus,
+    },
+    {
+        title: 'Minha Jam',
+        href: '/minha-jam',
+        icon: Music,
     },
 ];
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('minha-jam', function () {
+        return Inertia::render('minha-jam');
+    })->name('minha-jam');
 });
 
 require __DIR__.'/settings.php';
