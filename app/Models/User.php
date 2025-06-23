0b8f570496e635b3a2616037917167814057d06c
ArturFonsecaDev <sgsartur@hotmail.com>
2025-06-23 02:02:54 -0400

    feat: User now has the relation defined

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function jamRoom(){
+        return $this->hasOne(JamRoom::class, 'owner_id');
+    }
 }
